1ba4e0197117367af6ec1d5ab2ebd28fb44d9de2
Andrea Antonello <andrea.antonello@gmail.com>
2020-02-14 13:58:32 +0100

    run on updated ios

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/hydrologis/SOFTWARE/FLUTTER/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/hydrologis/development/DART/smash"
+export "FLUTTER_ROOT=/Users/smash/SOFTWARE/FLUTTER/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/smash/development/smash"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/hydrologis/SOFTWARE/FLUTTER/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=0.2.0"
-export "FLUTTER_BUILD_NUMBER=0"
+export "FLUTTER_FRAMEWORK_DIR=/Users/smash/SOFTWARE/FLUTTER/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
