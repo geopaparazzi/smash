f1e1c2f421eeca92ead99af12bfaa7d8142efdb5
Andrea Antonello <andrea.antonello@gmail.com>
2020-06-24 09:49:32 +0200

    fix export

--- lib/eu/hydrologis/smash/export/pdf_export.dart
+++ lib/eu/hydrologis/smash/export/pdf_export.dart
@@ -44,176 +44,193 @@ class PdfExporter {
 
     List<Note> simpleNotesList = db.getNotes(doSimple: true);
 
-    List<List<String>> simpleNotesTable = [
-      ['id', 'lon', 'lat', 'altim', 'text', 'date', 'accur', 'head', 'speed']
-    ];
-    simpleNotesList.forEach((note) {
-      List<String> row = [];
-      row.add(note.id.toString());
-      row.add(note.lon.toStringAsFixed(6));
-      row.add(note.lat.toStringAsFixed(6));
-      row.add("${note.altim.toInt()}m");
-      row.add(note.text);
-      row.add(TimeUtilities.ISO8601_TS_FORMATTER
-          .format(DateTime.fromMillisecondsSinceEpoch(note.timeStamp)));
-      var noteExt = note.noteExt;
+    List<List<String>> simpleNotesTable = [];
+    if (simpleNotesList.isNotEmpty) {
+      simpleNotesTable.add([
+        'id',
+        'lon',
+        'lat',
+        'altim',
+        'text',
+        'date',
+        'accur',
+        'head',
+        'speed'
+      ]);
+      simpleNotesList.forEach((note) {
+        List<String> row = [];
+        row.add(note.id.toString());
+        row.add(note.lon.toStringAsFixed(6));
+        row.add(note.lat.toStringAsFixed(6));
+        row.add("${note.altim.toInt()}m");
+        row.add(note.text);
+        row.add(TimeUtilities.ISO8601_TS_FORMATTER
+            .format(DateTime.fromMillisecondsSinceEpoch(note.timeStamp)));
+        var noteExt = note.noteExt;
 
-      if (noteExt != null) {
-        if (noteExt.accuracy != null) {
-          row.add("${noteExt.accuracy.toInt()}m");
+        if (noteExt != null) {
+          if (noteExt.accuracy != null) {
+            row.add("${noteExt.accuracy.toInt()}m");
+          } else {
+            row.add(novalue);
+          }
+          if (noteExt.heading != null) {
+            row.add("${noteExt.heading.toInt()}");
+          } else {
+            row.add(novalue);
+          }
+          if (noteExt.speed != null) {
+            row.add("${noteExt.speed.toStringAsFixed(1)}m/s");
+          } else {
+            row.add(novalue);
+          }
         } else {
           row.add(novalue);
-        }
-        if (noteExt.heading != null) {
-          row.add("${noteExt.heading.toInt()}");
-        } else {
+          row.add(novalue);
           row.add(novalue);
         }
-        if (noteExt.speed != null) {
-          row.add("${noteExt.speed.toStringAsFixed(1)}m/s");
-        } else {
-          row.add(novalue);
-        }
-      } else {
-        row.add(novalue);
-        row.add(novalue);
-        row.add(novalue);
-      }
-      simpleNotesTable.add(row);
-    });
-
-    List<Widget> formWidgetList = [];
-    formWidgetList.add(
-      Header(level: 1, text: 'Forms'),
-    );
+        simpleNotesTable.add(row);
+      });
+    }
 
     List<Note> formNotesList = db.getNotes(doSimple: false);
-    for (var i = 0; i < formNotesList.length; i++) {
-      Note note = formNotesList[i];
-      formWidgetList.add(Header(level: 2, text: note.text));
+    List<Widget> formWidgetList = [];
+    if (formNotesList.isNotEmpty) {
+      formWidgetList.add(
+        Header(level: 1, text: 'Forms'),
+      );
+      for (var i = 0; i < formNotesList.length; i++) {
+        Note note = formNotesList[i];
+        formWidgetList.add(Header(level: 2, text: note.text));
 
-      String formJson = note.form;
-      Map<String, dynamic> sectionMap = jsonDecode(formJson);
+        String formJson = note.form;
+        Map<String, dynamic> sectionMap = jsonDecode(formJson);
 //      var sectionName = sectionMap[ATTR_SECTIONNAME];
-      List<String> formNames = TagsManager.getFormNames4Section(sectionMap);
-      for (var j = 0; j < formNames.length; j++) {
-        var formName = formNames[j];
-        formWidgetList.add(Header(level: 3, text: formName));
+        List<String> formNames = TagsManager.getFormNames4Section(sectionMap);
+        for (var j = 0; j < formNames.length; j++) {
+          var formName = formNames[j];
+          formWidgetList.add(Header(level: 3, text: formName));
 
-        var form4name = TagsManager.getForm4Name(formName, sectionMap);
-        List<dynamic> formItems = TagsManager.getFormItems(form4name);
+          var form4name = TagsManager.getForm4Name(formName, sectionMap);
+          List<dynamic> formItems = TagsManager.getFormItems(form4name);
 
-        for (var k = 0; k < formItems.length; k++) {
-          var itemMap = formItems[k];
-          String label = TagsManager.getLabelFromFormItem(itemMap);
+          for (var k = 0; k < formItems.length; k++) {
+            var itemMap = formItems[k];
+            String label = TagsManager.getLabelFromFormItem(itemMap);
 
-          dynamic value = ""; //$NON-NLS-1$
-          if (itemMap.containsKey(TAG_VALUE)) {
-            value = itemMap[TAG_VALUE].trim();
-          }
-          String type = TYPE_STRING;
-          if (itemMap.containsKey(TAG_TYPE)) {
-            type = itemMap[TAG_TYPE].trim();
-          }
+            dynamic value = ""; //$NON-NLS-1$
+            if (itemMap.containsKey(TAG_VALUE)) {
+              value = itemMap[TAG_VALUE].trim();
+            }
+            String type = TYPE_STRING;
+            if (itemMap.containsKey(TAG_TYPE)) {
+              type = itemMap[TAG_TYPE].trim();
+            }
 
-          if (type == TYPE_PICTURES || type == TYPE_IMAGELIB) {
-            try {
-              var idSplit = value.toString().split(";");
-              for (var i = 0; i < idSplit.length; i++) {
-                var imageId = int.parse(idSplit[i]);
-                DbImage image = db.getImageById(imageId);
-                var p =
-                    Paragraph(text: image.text, textAlign: TextAlign.center);
-                formWidgetList.add(p);
+            if (type == TYPE_PICTURES || type == TYPE_IMAGELIB) {
+              try {
+                var valueString = value.toString().trim();
+                var idSplit = valueString.split(";");
+                if (valueString.isEmpty) {
+                  idSplit = [];
+                }
+                for (var i = 0; i < idSplit.length; i++) {
+                  var imageId = int.parse(idSplit[i]);
+                  DbImage image = db.getImageById(imageId);
+                  var p =
+                      Paragraph(text: image.text, textAlign: TextAlign.center);
+                  formWidgetList.add(p);
 
-                Uint8List imageDataBytes =
-                    db.getImageDataBytes(image.imageDataId);
-                List<int> resizeImage = ImageUtilities.resizeImage(
-                    imageDataBytes,
-                    longestSizeTo: EXPORT_IMG_LONGSIZE);
-                IMG.Image img = IMG.decodeImage(resizeImage);
+                  Uint8List imageDataBytes =
+                      db.getImageDataBytes(image.imageDataId);
+                  List<int> resizeImage = ImageUtilities.resizeImage(
+                      imageDataBytes,
+                      longestSizeTo: EXPORT_IMG_LONGSIZE);
+                  IMG.Image img = IMG.decodeImage(resizeImage);
 
-                final pdfImage = PdfImage(
-                  pdf.document,
-                  image: img.data.buffer.asUint8List(),
-                  width: img.width,
-                  height: img.height,
-                );
-                var c = Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Center(child: Image(pdfImage, fit: BoxFit.none)),
-                    ]);
-                formWidgetList.add(c);
+                  final pdfImage = PdfImage(
+                    pdf.document,
+                    image: img.data.buffer.asUint8List(),
+                    width: img.width,
+                    height: img.height,
+                  );
+                  var c = Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Center(child: Image(pdfImage, fit: BoxFit.none)),
+                      ]);
+                  formWidgetList.add(c);
+                }
+              } catch (e, s) {
+                SLogger().e("Error exporting image to pdf document", s);
               }
-            } catch (e, s) {
-              SLogger().e("Error exporting image to pdf document", s);
-            }
-          } else if (type == TYPE_DYNAMICSTRING) {
-            var p = Paragraph(text: '$label: ');
-            formWidgetList.add(p);
-            List<String> valueSplit = value.toString().split(";");
-            valueSplit.forEach((v) {
-              var b = Bullet(text: v);
-              formWidgetList.add(b);
-            });
-          } else if (type == TYPE_LABEL || type == TYPE_LABELWITHLINE) {
-            String sizeStr = "20";
-            if (itemMap.containsKey(TAG_SIZE)) {
-              sizeStr = itemMap[TAG_SIZE];
-            }
-            double size = double.parse(sizeStr);
+            } else if (type == TYPE_DYNAMICSTRING) {
+              var p = Paragraph(text: '$label: ');
+              formWidgetList.add(p);
+              List<String> valueSplit = value.toString().split(";");
+              valueSplit.forEach((v) {
+                var b = Bullet(text: v);
+                formWidgetList.add(b);
+              });
+            } else if (type == TYPE_LABEL || type == TYPE_LABELWITHLINE) {
+              String sizeStr = "20";
+              if (itemMap.containsKey(TAG_SIZE)) {
+                sizeStr = itemMap[TAG_SIZE];
+              }
+              double size = double.parse(sizeStr);
 
 //            var textDecoration;
 //            if(type==TYPE_LABELWITHLINE){
 //              textDecoration = TextDecoration.underline;
 //            }
 
-            var p = Paragraph(text: '$value', style: TextStyle(fontSize: size));
-            formWidgetList.add(p);
-          } else {
-            var p = Paragraph(text: '$label: $value');
-            formWidgetList.add(p);
+              var p =
+                  Paragraph(text: '$value', style: TextStyle(fontSize: size));
+              formWidgetList.add(p);
+            } else {
+              var p = Paragraph(text: '$label: $value');
+              formWidgetList.add(p);
+            }
           }
         }
-      }
 
-      formWidgetList.add(
-        Header(level: 1, text: 'Image notes'),
-      );
-
-      List<DbImage> images = db.getImages();
-      for (var i = 0; i < images.length; i++) {
-        DbImage image = images[i];
-
-        var p = Paragraph(text: image.text);
-        formWidgetList.add(p);
-        formWidgetList.addAll([
-          Bullet(text: "latitude: ${image.lat}"),
-          Bullet(text: "longitude: ${image.lon}"),
-          Bullet(text: "altimetry: ${image.altim}m"),
-          Bullet(text: "azimuth: ${image.azim}"),
-          Bullet(
-              text:
-                  "timestamp: ${TimeUtilities.ISO8601_TS_FORMATTER.format(DateTime.fromMillisecondsSinceEpoch(image.timeStamp))}"),
-        ]);
-        Uint8List imageDataBytes = db.getImageDataBytes(image.imageDataId);
-        List<int> resizeImage = ImageUtilities.resizeImage(imageDataBytes,
-            longestSizeTo: EXPORT_IMG_LONGSIZE);
-        IMG.Image img = IMG.decodeImage(resizeImage);
-
-        final pdfImage = PdfImage(
-          pdf.document,
-          image: img.data.buffer.asUint8List(),
-          width: img.width,
-          height: img.height,
+        formWidgetList.add(
+          Header(level: 1, text: 'Image notes'),
         );
-        var c = Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(child: Image(pdfImage, fit: BoxFit.none)),
-            ]);
-        formWidgetList.add(c);
+
+        List<DbImage> images = db.getImages();
+        for (var i = 0; i < images.length; i++) {
+          DbImage image = images[i];
+
+          var p = Paragraph(text: image.text);
+          formWidgetList.add(p);
+          formWidgetList.addAll([
+            Bullet(text: "latitude: ${image.lat}"),
+            Bullet(text: "longitude: ${image.lon}"),
+            Bullet(text: "altimetry: ${image.altim}m"),
+            Bullet(text: "azimuth: ${image.azim}"),
+            Bullet(
+                text:
+                    "timestamp: ${TimeUtilities.ISO8601_TS_FORMATTER.format(DateTime.fromMillisecondsSinceEpoch(image.timeStamp))}"),
+          ]);
+          Uint8List imageDataBytes = db.getImageDataBytes(image.imageDataId);
+          List<int> resizeImage = ImageUtilities.resizeImage(imageDataBytes,
+              longestSizeTo: EXPORT_IMG_LONGSIZE);
+          IMG.Image img = IMG.decodeImage(resizeImage);
+
+          final pdfImage = PdfImage(
+            pdf.document,
+            image: img.data.buffer.asUint8List(),
+            width: img.width,
+            height: img.height,
+          );
+          var c = Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(child: Image(pdfImage, fit: BoxFit.none)),
+              ]);
+          formWidgetList.add(c);
+        }
       }
     }
 
@@ -246,22 +263,38 @@ class PdfExporter {
                       .defaultTextStyle
                       .copyWith(color: PdfColors.grey)));
         },
-        build: (Context context) => <Widget>[
+        build: (Context context) {
+          List<Widget> finalWidgets = []..add(
               Header(
                   level: 0,
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         Image(smashLogo, fit: BoxFit.scaleDown),
-                        Text('Project: $dbName',
-                            textScaleFactor: 2, softWrap: true),
+                        FittedBox(
+                          child: Text('Project: $dbName',
+                              textScaleFactor: 2, softWrap: true),
+                        )
                       ])),
+            );
+
+          if (simpleNotesTable.isNotEmpty) {
+            finalWidgets.add(
               Header(level: 1, text: 'Simple Notes'),
+            );
+            finalWidgets.add(
               Table.fromTextArray(
                 context: context,
                 data: simpleNotesTable,
               ),
-            ]..addAll(formWidgetList)));
+            );
+          }
+          if (formWidgetList.isNotEmpty) {
+            finalWidgets.addAll(formWidgetList);
+          }
+
+          return finalWidgets;
+        }));
 
     await outputFile.writeAsBytes(pdf.save());
   }
