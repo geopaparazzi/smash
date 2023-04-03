242510f4638e4267bd4a9e3167dbd3d0d0db8e2e
Andrea Antonello <andrea.antonello@gmail.com>
2023-04-03 17:16:22 +0200

    modify sed scripts for play store handling

--- play_store_unfixes.sh
+++ play_store_unfixes.sh
@@ -1,2 +1,2 @@
-sed -i 's/^.*PLAY STORE FIX.*$/          .add(PERMISSIONS.MANAGEEXTSTORAGE)/' lib/main.dart
+sed -i 's/forStore = true/forStore = false/' lib/main.dart
 sed -i 's/<!-- <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" \/> -->/<uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" \/>/' android/app/src/main/AndroidManifest.xml
