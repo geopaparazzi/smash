4eab0adf08d508f4485857513b6868bf6cb15da4
Andrea Antonello <andrea.antonello@gmail.com>
2023-05-25 15:49:49 +0200

    upgraded and fixed libs deps

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
-import package_info_plus_macos
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqlite3_flutter_libs
