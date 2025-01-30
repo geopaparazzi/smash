7055f2cd0278986380c4d9fde3f3f36f784d28b6
Andrea Antonello <andrea.antonello@gmail.com>
2025-01-30 10:43:17 +0100

    remove unused + deps up

--- play_store_fixes.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/forStore = false/forStore = true/' lib/main.dart
-sed -i 's/<uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" \/>/<!-- & -->/' android/app/src/main/AndroidManifest.xml
